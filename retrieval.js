bee815b617210a8c16824f5aee3d9db51e1b7c85
jferrin1996 <jakeferrin1996@gmail.com>
2020-05-07 09:56:19 -0500

    Final Presentation

--- retrieval.js
+++ retrieval.js
@@ -1,12 +1,19 @@
-var docRef = db.collection("debt").doc("totalDebt");
+let userRef = db.collection('users').doc('user1');
+let getDoc = cityRef.get()
+    .then(doc => {
+        if(!doc.exists){
+            console.log('No such document!');
+        } else {
+            console.log('Document Data: ',doc.data());
+        }
+    })
+    .catch(err => {
+        console.log('Error getting document',err);
+    })
 
-docRef.get(debt).then(function(doc) {
-    if (doc.exists) {
-        console.log("Document data:", doc.data());
-    } else {
-        // doc.data() will be undefined in this case
-        console.log("No such document!");
-    }
-}).catch(function(error) {
-    console.log("Error getting document:", error);
-});
+db.collection("users")
+.doc("user1")
+.get()
+.then(doc => {
+    console.log(doc.data())
+})
